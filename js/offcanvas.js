16ea37934ca21142729feb8179b546b3631a2241
alfianjau <alfianjauhari@gmail.com>
2015-08-10 18:51:16 +0700

    mod js
    alter javascript

--- js/offcanvas.js
+++ js/offcanvas.js
@@ -2,19 +2,3 @@
  * Created by Alfianjau on 30/07/2015.
  */
 $(document).foundation();
-
-
-
-$(function() {
-
-    $(".right-off-canvas-toggle, .exit-off-canvas").click(function() {
-
-        $(".top-bar").toggleClass("top-bar-close");
-
-        $(".middle-bar").toggleClass("middle-bar-close");
-
-        $(".bottom-bar").toggleClass("bottom-bar-close");
-
-    });
-
-});
